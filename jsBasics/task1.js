c5d5e362a3f202ebb6ae54df64f514fdfdf90f64
vrybachenko <rybachenko.vadim@gmail.com>
2026-01-12 22:20:12 +0200

    js primitive data types homework

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+const newString = "Hello World!";
+const newNumber = 12;
+const newBoolean = true;
+const newNull = null;
+let newUndefined; //undefined by default
+
+console.log("String:", newString);
+console.log("Number:", newNumber);
+console.log("Boolean:", newBoolean);
+console.log("Null:", newNull);
+console.log("Undefined:", newUndefined);
---
+++ jsBasics/task3.js
@@ -0,0 +1,11 @@
+const age = 18;
+const isAdult = age >= 18;
+
+console.log("Age:", age);
+console.log("Is adult:", isAdult);
+
+if (isAdult) {
+    console.log("This person can drink a beer :)");
+} else {
+    console.log("This person has age restriction for buying beer");
+}
